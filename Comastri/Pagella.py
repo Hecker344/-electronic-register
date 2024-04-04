2d6aed69abbeeb40acbc3db550270dbf37301851
mikicoma <164473304+mikicoma@users.noreply.github.com>
2024-04-04 19:29:55 +0200

    Update Pagella.py

--- Comastri/Pagella.py
+++ Comastri/Pagella.py
@@ -2,27 +2,28 @@ def Pagella(aula):
     classe = 'terza_h'
     cognome = 'gazzotti'
     medie = []
-    somma = 0
     materie = ['matematica','italiano','storia','informatica','inglese']
-    if classe in aula and cognome in aula[classe]:
-        for x in range(5):
-            voti = aula[classe][cognome][materie[x]]
-            somma = sum(voti)
+    if classe in aula and cognome in aula[classe]: #Controlliamo che lo studente esista
+        for x in range(5): #usiamo un for range di 5 dato che ci sono 5 materie
+            somma=0 #azzeriamo somma per i cicli successivi
+            voti = aula[classe][cognome][materie[x]] #Andiamo in ogni materia utilizzando x
+            for y in voti:
+                somma=somma+y #sommiamo e facciamo la media per ogni materia
             media=somma/len(voti)
-            medie.append(media)
+            medie.append(media) #aggiungiamo il risultato media a una lista(medie)
     else:
-        print("Cognome non trovato")
+        print("Cognome non trovato")#Nel caso in cui il cognome non esista comparirà un avviso
     print(medie)
     colori=[]
     for z in medie:
-        if z<=5.5:
+        if z<=5.5: #utilizziamo una serie di if per attribuire a ogni voto un colore che verrà aggiunto a una lista(colori)
             colori.append('red')
         elif z>5.51 and z<=6:
             colori.append('yellow')
         else:
             colori.append('green')
     print(colori)
-    plt.bar(materie,medie,color=colori)
+    plt.bar(materie,medie,color=colori) #utillizziamo matplot per creare un grafico a barre
     plt.title('Pagella')
     plt.xlabel('Materie')
     plt.ylabel('Voti')
