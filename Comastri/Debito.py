b819043ff8cfe3f05157b40d6ee0d1b5ff1117b2
mikicoma <164473304+mikicoma@users.noreply.github.com>
2024-04-04 19:51:01 +0200

    Update Debito.py

--- Comastri/Debito.py
+++ Comastri/Debito.py
@@ -7,8 +7,3 @@ def Debito():
             media = sum(voti)/len(voti) # facciamo la media di ogni studente
             if media < 6: # in caso in qui lo studente abbia la media inferiore al 6 lo stampiamo
                 print(y,"ha la media insufficente")
-
-
-
-
-Debito()
