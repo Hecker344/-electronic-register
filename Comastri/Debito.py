27e11656bb4d71b005715126a105226ba0a673e5
mikicoma <164473304+mikicoma@users.noreply.github.com>
2024-04-04 19:17:07 +0200

    Update Debito.py

--- Comastri/Debito.py
+++ Comastri/Debito.py
@@ -2,10 +2,10 @@ def Debito():
     materia_prof = "matematica"
     for x in classi:
         for y in classi[x]:
-            voti=classi[x][y][materia_prof]
+            voti=classi[x][y][materia_prof] #andiamo a fare tutti i possibili casi di studenti
             somma = 0
-            media = sum(voti)/len(voti)
-            if media < 6:
+            media = sum(voti)/len(voti) # facciamo la media di ogni studente
+            if media < 6: # in caso in qui lo studente abbia la media inferiore al 6 lo stampiamo
                 print(y,"ha la media insufficente")
 
 
